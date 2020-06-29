08f7e96ddb13ae2ba5c35e1efe7ab94fad5bf180
Vlad Ionica <mihaiionica10@gmail.com>
2020-06-29 15:36:32 +0100

    Added Database Paths to the RestService class

--- app/src/main/java/com/example/aida/dal/RestService.java
+++ app/src/main/java/com/example/aida/dal/RestService.java
@@ -5,6 +5,12 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class RestService {
     protected FirebaseFirestore database;
 
+    protected String usersPath = "users";
+    protected String citiesPath = "cities";
+    protected String countriesPath = "countries";
+    protected String entriesPath = "entries";
+    protected String foodsPath = "foods";
+
     public RestService(){
         database = FirebaseFirestore.getInstance();
     }
