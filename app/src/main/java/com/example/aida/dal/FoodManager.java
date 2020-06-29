e34797ee4c9fe3b8ef93506a12c65877d3b428d2
Vlad Ionica <mihaiionica10@gmail.com>
2020-06-29 16:32:31 +0100

    Implemented Read Method for the FoodManager Class

--- app/src/main/java/com/example/aida/dal/FoodManager.java
+++ app/src/main/java/com/example/aida/dal/FoodManager.java
@@ -1,10 +1,47 @@
 package com.example.aida.dal;
 
+import androidx.annotation.NonNull;
+
+import com.example.aida.models.Food;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class FoodManager extends RestService {
     //TODO: Add ViewModel Constructor
 
     // Used for Getting Foods from the Database
     public void read(){
+        database.collection(foodsPath).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    ArrayList<Food> foods = new ArrayList<>();
+                    List<DocumentSnapshot> documents = task.getResult().getDocuments();
+                    for(DocumentSnapshot document : documents){
+                        String id = document.getId();
+                        String name = document.getData().get("name").toString();
+                        String category = document.getData().get("category").toString();
+                        float carbs = Float.valueOf(document.getData().get("carbohydrates").toString());
+                        float sugars = Float.valueOf(document.getData().get("sugars").toString());
+                        float fat = Float.valueOf(document.getData().get("fat").toString());
+                        float saturates = Float.valueOf(document.getData().get("saturates").toString());
+                        float energy = Float.valueOf(document.getData().get("energy").toString());
+                        float fiber = Float.valueOf(document.getData().get("fiber").toString());
+                        float protein = Float.valueOf(document.getData().get("protein").toString());
+                        float salt = Float.valueOf(document.getData().get("salt").toString());
 
+                        Food food = new Food(id, name, category, carbs, sugars, fat, saturates, energy, fiber, protein, salt);
+                        foods.add(food);
+                    }
+
+                    //TODO: Add Callback Method
+                }
+            }
+        });
     }
 }
