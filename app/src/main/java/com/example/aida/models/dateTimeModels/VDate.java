a4dd9bef5456a077d4a69dc3fdf34b7bae126d32
Vlad Ionica <mihaiionica10@gmail.com>
2020-07-05 04:07:37 +0100

    Improved VDate and VTime Constructors by adding safety checks to ensure the data is a valid Date/Time format

--- app/src/main/java/com/example/aida/models/dateTimeModels/VDate.java
+++ app/src/main/java/com/example/aida/models/dateTimeModels/VDate.java
@@ -6,25 +6,82 @@ public class VDate {
     private int year;
 
     public VDate(){
-        this.day = 0;
-        this.month = 0;
-        this.year = 0;
+        day = 1;
+        month = 1;
+        year = 1;
     }
 
     public VDate(String date){
-        this.day = Integer.valueOf(String.valueOf(date.charAt(0)) + String.valueOf(date.charAt(1)));
-        this.month = Integer.valueOf(String.valueOf(date.charAt(3)) + String.valueOf(date.charAt(4)));
-        if(date.length() == 8)
-            this.year = Integer.valueOf(String.valueOf(date.charAt(6)) + String.valueOf(date.charAt(7)));
-        else
-            this.year = Integer.valueOf(String.valueOf(date.charAt(6)) + String.valueOf(date.charAt(7)) +
-                    String.valueOf(date.charAt(8)) + String.valueOf(date.charAt(9)));
+        if(date.length() == 10) {
+            day = Integer.valueOf(String.valueOf(date.charAt(0)) + String.valueOf(date.charAt(1)));
+            month = Integer.valueOf(String.valueOf(date.charAt(3)) + String.valueOf(date.charAt(4)));
+            year = Integer.valueOf(String.valueOf(date.charAt(6)) + String.valueOf(date.charAt(7)) + String.valueOf(date.charAt(8)) + String.valueOf(date.charAt(9)));
+
+            if(year < 1) {
+                year = 1;
+            } else if(year > 9999) {
+                year = 9999;
+            }
+
+            if(month < 1) {
+                month = 1;
+            } else if(month > 12) {
+                month = 12;
+            }
+
+            if(day < 1) {
+                day = 1;
+            } else if(day > 28 && month == 2 && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))) {
+                day = 28;
+            } else if(day > 29 && month == 2 && (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))) {
+                day = 29;
+            } else if(day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+                day = 30;
+            } else if(day > 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+                day = 31;
+            }
+
+        } else if(date.length() == 8) {
+            day = Integer.valueOf(String.valueOf(date.charAt(0)) + String.valueOf(date.charAt(1)));
+            month = Integer.valueOf(String.valueOf(date.charAt(3)) + String.valueOf(date.charAt(4)));
+            year = Integer.valueOf(String.valueOf(date.charAt(6)) + String.valueOf(date.charAt(7)));
+        } else {
+            day = 1;
+            month = 1;
+            year = 1;
+        }
     }
 
     public VDate(int day, int month, int year) {
-        this.day = day;
-        this.month = month;
-        this.year = year;
+        if(year < 1) {
+            this.year = 1;
+        } else if(year > 9999) {
+            this.year = 9999;
+        } else {
+            this.year = year;
+        }
+
+        if(month < 1) {
+            this.month = 1;
+        } else if(month > 12) {
+            this.month = 12;
+        } else {
+            this.month = month;
+        }
+
+        if(day < 1) {
+            this.day = 1;
+        } else if(day > 28 && month == 2 && (year % 4 != 0 || (year % 100 == 0 && year % 400 != 0))) {
+            this.day = 28;
+        } else if(day > 29 && month == 2 && (year % 4 == 0 || (year % 100 == 0 && year % 400 == 0))) {
+            this.day = 29;
+        } else if(day > 30 && (month == 4 || month == 6 || month == 9 || month == 11)) {
+            this.day = 30;
+        } else if(day > 31 && (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)) {
+            this.day = 31;
+        } else {
+            this.day = day;
+        }
     }
 
     public int getDay() { return day; }
--- app/src/main/java/com/example/aida/models/dateTimeModels/VTime.java
+++ app/src/main/java/com/example/aida/models/dateTimeModels/VTime.java
@@ -6,21 +6,83 @@ public class VTime {
     private int hour;
 
     public VTime() {
-        this.second = 0;
-        this.minute = 0;
-        this.hour = 0;
+        second = 0;
+        minute = 0;
+        hour = 0;
     }
 
     public VTime(String time){
-        this.hour = Integer.valueOf(String.valueOf(time.charAt(0)) + String.valueOf(time.charAt(1)));
-        this.minute = Integer.valueOf(String.valueOf(time.charAt(3)) + String.valueOf(time.charAt(4)));
-        this.second = 0;
+        if(time.length() == 8) {
+            second = Integer.valueOf(String.valueOf(time.charAt(6)) + String.valueOf(time.charAt(7)));
+            minute = Integer.valueOf(String.valueOf(time.charAt(3)) + String.valueOf(time.charAt(4)));
+            hour = Integer.valueOf(String.valueOf(time.charAt(0)) + String.valueOf(time.charAt(1)));
+
+            if(second < 0){
+                this.second = 0;
+            } else if(second > 59) {
+                this.second = 59;
+            }
+
+            if(minute < 0) {
+                this.minute = 0;
+            } else if(minute > 59) {
+                this.minute = 59;
+            }
+
+            if(hour < 0) {
+                this.hour = 0;
+            } else if(hour > 23) {
+                this.hour = 23;
+            }
+
+        } else if(time.length() == 5) {
+            second = 0;
+            minute = Integer.valueOf(String.valueOf(time.charAt(3)) + String.valueOf(time.charAt(4)));
+            hour = Integer.valueOf(String.valueOf(time.charAt(0)) + String.valueOf(time.charAt(1)));
+
+            if(minute < 0) {
+                this.minute = 0;
+            } else if(minute > 59) {
+                this.minute = 59;
+            }
+
+            if(hour < 0) {
+                this.hour = 0;
+            } else if(hour > 23) {
+                this.hour = 23;
+            }
+
+        } else {
+            second = 0;
+            minute = 0;
+            hour = 0;
+        }
     }
 
     public VTime(int second, int minute, int hour) {
-        this.second = second;
-        this.minute = minute;
-        this.hour = hour;
+        if(second < 0){
+            this.second = 0;
+        } else if(second > 59) {
+            this.second = 59;
+        } else {
+            this.second = second;
+        }
+
+        if(minute < 0) {
+            this.minute = 0;
+        } else if(minute > 59) {
+            this.minute = 59;
+        } else {
+            this.minute = minute;
+        }
+
+        if(hour < 0) {
+            this.hour = 0;
+        } else if(hour > 23) {
+            this.hour = 23;
+        } else {
+            this.hour = hour;
+        }
     }
 
     public int getSecond() { return second; }
